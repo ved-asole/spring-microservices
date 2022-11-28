3eafcdcd7888f69615ee5094e3395fa38a75fdbf
Ved Asole <vedasole001@gmail.com>
2022-11-29 02:51:43 +0530

    added basic limit-controller

---
+++ limits-service/src/main/java/com/codersarena/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,17 @@
+package com.codersarena.microservices.limitsservice.controller;
+
+import com.codersarena.microservices.limitsservice.bean.Limits;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits() {
+
+        return new Limits(1, 1000);
+
+    }
+
+}
