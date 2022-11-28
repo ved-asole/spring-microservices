229d21299c33f3ccf86e4c04e040d93001af1eac
Ved Asole <vedasole001@gmail.com>
2022-11-29 02:30:51 +0530

    initial-commit

---
+++ limits-service/src/test/java/com/codersarena/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codersarena.microservices.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
