366cacb03571202ecdc5c96fd1767b72acb8f4b2
Ved Asole <vedasole001@gmail.com>
2022-12-01 00:55:59 +0530

    updated to fetch currency exchanges from db

--- currency-exchange-service/src/main/java/com/codersarena/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/codersarena/currencyexchangeservice/CurrencyExchangeController.java
@@ -5,7 +5,6 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
@@ -13,18 +12,22 @@ public class CurrencyExchangeController {
     @Autowired
     private Environment environment;
 
+    @Autowired
+    private CurrencyExchangeRepository repository;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(
             @PathVariable String from,
             @PathVariable String to
     ){
 
-        CurrencyExchange currencyExchange = new CurrencyExchange(
-                1000L,
-                from,
-                to,
-                BigDecimal.valueOf(50)
-        );
+        CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+
+        if(currencyExchange == null){
+            throw new RuntimeException(
+                    "Unable to find data from " + from + " to " + to
+            );
+        }
 
         String port = environment.getProperty("local.server.port");
         currencyExchange.setEnvironment(port);
---
+++ currency-exchange-service/src/main/java/com/codersarena/currencyexchangeservice/CurrencyExchangeRepository.java
@@ -0,0 +1,9 @@
+package com.codersarena.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+
+    CurrencyExchange findByFromAndTo(String from, String to);
+
+}
