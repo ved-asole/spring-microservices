b51cd2150d720968a7425b2449c4f3f3dce03fed
Ved Asole <vedasole001@gmail.com>
2022-11-30 14:19:13 +0530

    added the h2 db jpa configuration

--- currency-exchange-service/src/main/java/com/codersarena/currencyexchangeservice/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/codersarena/currencyexchangeservice/CurrencyExchange.java
@@ -1,11 +1,19 @@
 package com.codersarena.currencyexchangeservice;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 import java.math.BigDecimal;
 
+@Entity
 public class CurrencyExchange {
 
+    @Id
     private long id;
+    @Column(name = "currency_from")
     private String from;
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private String environment;
