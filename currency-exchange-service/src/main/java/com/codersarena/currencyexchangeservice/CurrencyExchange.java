c1af7b5b3713b8f90c743199d59ebfaf4ba701cb
Ved Asole <vedasole001@gmail.com>
2022-11-30 13:34:37 +0530

    added the Currency Exchange api

---
+++ currency-exchange-service/src/main/java/com/codersarena/currencyexchangeservice/CurrencyExchange.java
@@ -0,0 +1,71 @@
+package com.codersarena.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+public class CurrencyExchange {
+
+    private long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+    private String environment;
+
+    public CurrencyExchange() { }
+
+    public CurrencyExchange(long id, String from, String to, BigDecimal conversionMultiple) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public BigDecimal getConversionMultiple() {
+        return conversionMultiple;
+    }
+
+    public void setConversionMultiple(BigDecimal conversionMultiple) {
+        this.conversionMultiple = conversionMultiple;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public String toString() {
+        return "CurrencyExchange{" +
+                "id=" + id +
+                ", from='" + from + '\'' +
+                ", to='" + to + '\'' +
+                ", conversionMultiple=" + conversionMultiple +
+                '}';
+    }
+}
---
+++ currency-exchange-service/src/main/java/com/codersarena/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,36 @@
+package com.codersarena.currencyexchangeservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(
+            @PathVariable String from,
+            @PathVariable String to
+    ){
+
+        CurrencyExchange currencyExchange = new CurrencyExchange(
+                1000L,
+                from,
+                to,
+                BigDecimal.valueOf(50)
+        );
+
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+
+        return currencyExchange;
+
+    }
+
+}
