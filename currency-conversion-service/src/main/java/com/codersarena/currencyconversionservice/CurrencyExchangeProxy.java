60d065fb867f246115b8f04a26515fd1998f673c
Ved Asole <vedasole001@gmail.com>
2022-12-02 14:46:16 +0530

    updated the microservice names

--- currency-conversion-service/src/main/java/com/codersarena/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/codersarena/currencyconversionservice/CurrencyExchangeProxy.java
@@ -4,7 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service", url = "http://localhost:8000")
+//@FeignClient(name = "currency-exchange-service", url = "http://localhost:8000")
+@FeignClient("currency-exchange")
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
