4aec68d469b783c38582853ccb82ef1d6de46590
Ved Asole <vedasole001@gmail.com>
2022-12-10 15:55:25 +0530

    added resilience4j retry logic to api

--- currency-exchange-service/src/main/java/com/codersarena/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/codersarena/currencyexchangeservice/CircuitBreakerController.java
@@ -1,14 +1,32 @@
 package com.codersarena.currencyexchangeservice;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
+    public Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
+    @Retry(name="sample-api", fallbackMethod = "failbackResponse")
     @GetMapping("/sample-api")
     public String sampleApi()  {
-        return "sample-response";
+        logger.info("Received the API call");
+
+        ResponseEntity<String> response = new RestTemplate()
+                .getForEntity(
+                        "http://localhost:8080/some-dummy-url",
+                        String.class
+                );
+        return response.getBody();
+    }
+
+    public String failbackResponse(Exception ex){
+        return "Failback Response";
     }
 
 }
